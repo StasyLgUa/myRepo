58893ba3b205006bfc913aaeb18ef58a97d8f153
StasyLgUa <stasy.contact@gmail.com>
2017-12-15 01:32:15 +0200

    lesson_3

---
+++ university/lesson_2/test3.py
@@ -0,0 +1,95 @@
+import math
+
+
+#x=3
+#y=5
+#print(x, y, 11.0, 'Spams')
+#print(x, y, sep=', ')
+#print('fdf', 'eeeee', 'popoo', sep='\n', end='\n')
+#print('fdf', 'eeeee', 'popoo', sep='')
+
+
+#print(x, y, sep=', ', end=', ')
+#print(11.0, 'Spams')
+
+
+#x = 10
+#while x>0:
+#    print(str(x) + '!')
+#    x-=1
+
+
+l = [1, 10, 3, 5, 6, 7, 11]
+#while l:
+#    l.pop(0)
+#    print(l)
+
+
+# c = 10
+# while c:
+#     print(c)
+#     c-=3
+#     if c==-2:
+#         print("c=",c)
+#         break
+
+
+# for p in l:
+#     if p % 2 == 0:
+#         print(p)
+#     else:
+#         print(p+1)
+#
+
+
+# for p in l:
+#     if p % 2 !=0:
+#         pp = p+1
+#         print(pp.insert(p, l))
+
+
+"""
+with counter
+"""
+# x=0
+# for p in l:
+#     if p % 2 != 0:
+#         l[x] = p+1
+#         print(l)
+#     x+=1
+#     print(l)
+#
+
+
+# for i in range(len(l)):
+#     #print(i, l[i])
+#     if l[i] % 2 !=0:
+#         l[i]+=1
+# print(l)
+
+
+# def rep(s, exclaim):
+#     pass
+
+
+
+# def summ(cat_1, cat_2):
+#
+#     if type(cat_1) in (int, float) and type(cat_2) in (int, float):
+#             return math.sqrt(cat_1**2 + cat_2**2)
+#     else:
+#             return
+#
+# r = summ(179, 971)
+# print(r)
+
+
+
+def st(mas):
+    for i in range(len(mas)):
+        if type(mas[i]) is (str):
+
+        return False
+
+s = ["fdfd", "fdfsaa", 43434]
+st(s)
